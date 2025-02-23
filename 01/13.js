37fb07599157a7a4e69683cfcc62076a0a02b8b7
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-23 12:03:02 +0530

    tests

---
+++ 01/13.js
@@ -0,0 +1,8 @@
+// set timeout
+
+function greet() {
+  console.log("hello everyone");
+}
+
+setTimeout(greet, 2 * 1000);
+//In this code, the callback function is greet because it is passed to setTimeout to be executed after a 2-second delay.
