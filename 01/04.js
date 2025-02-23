2bee3a90b28a0fa3fe2ee2b6163eb9b97464a3f0
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-23 11:33:47 +0530

    tests

---
+++ 01/04.js
@@ -0,0 +1,11 @@
+// data types in JS
+
+let firstName = "Steve";
+let lastName = "Rogers";
+
+let age = 90;
+let ismale = true;
+
+console.log(
+  `Hello ${firstName} ${lastName}, you are ${age} years old and your gender is Male ${ismale}`
+);
