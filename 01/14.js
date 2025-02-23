4836da836244d8b3977a2c4835aa13d6f3b5768c
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-23 12:03:36 +0530

    tests

---
+++ 01/14.js
@@ -0,0 +1,8 @@
+// set interval
+
+function greet() {
+  console.log("hello everyone");
+}
+
+setInterval(greet, 2 * 1000);
+//In this code, the callback function is greet because it is passed to setInterval to be executed after a 2-second delay.
