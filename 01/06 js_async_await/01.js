c46bc84bd97c3cd981689e71934d2b20007bd105
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-02-27 11:34:33 +0530

    tests

--- 01/06 js_async_await/01.js
+++ 01/06 js_async_await/01.js
@@ -1,10 +1,10 @@
 // async await syntax
 
-const fs = require("fs").promises;
+import { readFile } from "fs/promises";
 
 async function readFileAsync(filePath, encoding = "utf-8") {
   try {
-    const data = await fs.readFile(filePath, encoding);
+    const data = await readFile(filePath, { encoding });
     return data;
   } catch (err) {
     throw err;
